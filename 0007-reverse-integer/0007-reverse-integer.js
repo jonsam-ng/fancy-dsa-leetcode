db2aaf2fca548fa5789baa288725113c9f4c5713
jonsam ng <39360338+jonsam-ng@users.noreply.github.com>
2022-11-01 18:10:45 +0800

    Time: 77 ms (93.47%), Space: 42.8 MB (99.63%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -1,11 +1,8 @@
 var reverse = function (x) {
-  let t = 0;
-
-  while (x !== 0) {
-    t = t * 10 + x % 10;
-    x = ~~(x / 10);
-  }
-
-  if (t > Math.pow(2, 31) - 1 || t < -Math.pow(2, 31)) return 0;
-  return t;
+  const max = Math.pow(2, 31),
+        isMinus = x < 0;
+  if (Math.abs(x) < 10) return x;
+  const n = Number(Math.abs(x).toString().split("").reverse().join(""));
+  if (n < -max || n > max - 1) return 0;
+  return isMinus ? -n : n;
 };

59e24e3189555442777f74f93d6dc73c1031d079
jonsam ng <39360338+jonsam-ng@users.noreply.github.com>
2022-11-21 14:12:21 +0800

    Time: 138 ms (14.47%), Space: 45.3 MB (7.59%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.js
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.js
@@ -1,6 +1,7 @@
 var flatten = function (root) {
   if (!root) return null;
-  const [left, right] = [root.left, root.right].map(head => head ? flatten(head) : null).reduce(arr => arr, [root.left, root.right]);
+  [root.left, root.right].map(head => head ? flatten(head) : null);
+  const [left, right] = [root.left, root.right];
   [root.left, root.right] = [null, left];
   let p = root;
 

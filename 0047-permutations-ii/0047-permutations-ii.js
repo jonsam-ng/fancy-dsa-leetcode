a40d47e2c6e900efadc48f7cf135a5eaf0bee612
Jonsam ng <jonsam.ng@foxmail.com>
2022-12-04 18:30:46 +0800

    Time: 126 ms (71.65%), Space: 44.3 MB (95.32%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.js
+++ 0047-permutations-ii/0047-permutations-ii.js
@@ -1,19 +1,21 @@
 var permuteUnique = function (nums) {
-  const sortedNums = nums.sort(),
-        len = nums.length,
-        r = [],
-        s = [],
-        visited = new Array(len).fill(0),
-        resolve = () => {
-    if (s.length === len) return r.push([...s]);
+  const len = nums.length,
+    sortedNums = nums.sort(),
+    s = [],
+    r = [],
+    visited = new Array(len).fill(0),
+    resolve = () => {
+      if (s.length === len) return r.push([...s]);
+      let prev = null;
 
-    for (let i = 0; i < len; i++) {
-      if (visited[i] || i > 0 && sortedNums[i] === sortedNums[i - 1] && !visited[i - 1]) continue;
-      s.push(sortedNums[i]), visited[i] = 1;
-      resolve(i + 1);
-      s.pop(), visited[i] = 0;
-    }
-  };
+      for (let i = 0; i < len; i++) {
+        const n = sortedNums[i];
+        if (visited[i] || prev === n) continue;
+        s.push((prev = n)), (visited[i] = 1);
+        resolve(i + 1);
+        s.pop(), (visited[i] = 0);
+      }
+    };
 
   return resolve(), r;
 };

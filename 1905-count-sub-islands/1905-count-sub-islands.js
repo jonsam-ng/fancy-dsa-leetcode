3e580d53cbfb5f126e015a447749c717439c1054
Jonsam ng <jonsam.ng@foxmail.com>
2022-12-04 21:06:49 +0800

    Time: 404 ms (35.06%), Space: 73.5 MB (41.96%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.js
+++ 1905-count-sub-islands/1905-count-sub-islands.js
@@ -24,3 +24,35 @@ var countSubIslands = function (grid1, grid2) {
 
   return resolve(), r;
 };
+
+// 不同形状岛屿的数量
+var numDistinctIslands = function (grid) {
+  const set = new Set(),
+    m = grid.length,
+    n = grid[0].length,
+    dirs = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ],
+    flood = (i, j, dir = "", s = []) => {
+      if (i < 0 || i >= m || j < 0 || j >= n || !grid[i][j]) return;
+      grid[i][j] = 0;
+      s.push(dir);
+      dirs.forEach(([x, y], idx) => flood(i + x, j + y, idx, s));
+      s.push(-dir);
+    },
+    resolve = () => {
+      for (let i = 0; i < m; i++)
+        for (let j = 0; j < n; j++) {
+          if (grid[i][j]) {
+            const shape = [];
+            flood(i, j, "", shape);
+            set.add(shape.join(""));
+          }
+        }
+    };
+
+  return resolve(), set.size;
+};

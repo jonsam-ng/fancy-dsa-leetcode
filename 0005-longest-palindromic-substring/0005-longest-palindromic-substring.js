325198bdc5cf112cedfe14bc9631488caeaa45ae
Jonsam ng <jonsam.ng@foxmail.com>
2022-12-07 20:48:09 +0800

    Time: 147 ms (66.53%), Space: 45 MB (66.47%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -1,19 +1,18 @@
 var longestPalindrome = function (s) {
   const len = s.length;
-  let lp = "";
-
-  function palindrome(l, r) {
+  let rst = "";
+  const palindrome = (l, r) => {
     while (l >= 0 && r < len && s[l] === s[r]) l--, r++;
-
     return s.substring(l + 1, r);
-  }
+  };
 
   for (let i = 0; i < len; i++) {
     const s1 = palindrome(i, i),
-          s2 = palindrome(i, i + 1),
-          ms = s1.length >= s2.length ? s1 : s2;
-    if (ms.length > lp.length) lp = ms;
+      s2 = palindrome(i, i + 1),
+      longer = s1.length >= s2.length ? s1 : s2;
+
+    if (longer.length > rst.length) rst = longer;
   }
 
-  return lp;
+  return rst;
 };

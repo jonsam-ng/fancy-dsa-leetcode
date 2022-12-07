e377b93d420f8ce7f8dff7e295006ecc3159acc1
Jonsam ng <jonsam.ng@foxmail.com>
2022-12-07 12:34:24 +0800

    Time: 126 ms (28.10%), Space: 46.2 MB (6.99%) - LeetHub

--- 0415-add-strings/0415-add-strings.js
+++ 0415-add-strings/0415-add-strings.js
@@ -6,8 +6,8 @@
 var addStrings = function (num1, num2) {
   let sum = "",
     i = num1.length - 1,
-    j = num2.length - 1;
-  overflow = 0;
+    j = num2.length - 1,
+    overflow = 0;
   const getNum = (i, nums) => (nums[i] !== undefined ? +nums[i] : null);
   // add pieces to sum
   while (i >= 0 || j >= 0) {
@@ -16,7 +16,7 @@ var addStrings = function (num1, num2) {
     const cur = [n1, n2, overflow]
       .filter((n) => n !== null)
       .reduce((sum, n) => (sum += n), 0);
-    sum = cur % 10 + sum;
+    sum = (cur % 10) + sum;
     overflow = ~~(cur / 10);
     i--, j--;
   }

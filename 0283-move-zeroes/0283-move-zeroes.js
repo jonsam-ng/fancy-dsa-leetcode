0c2e226517045f81a9b9ef875e71c60192cf2237
Jonsam ng <jonsam.ng@foxmail.com>
2022-12-09 00:06:29 +0800

    Time: 157 ms (52.55%), Space: 46.7 MB (59.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -1,15 +1,11 @@
 var moveZeroes = function (nums) {
   const len = nums.length;
-  if (!len) return nums;
   let slow = 0,
-      fast = 0;
-
+    fast = 0;
   while (fast < len) {
     if (nums[fast]) nums[slow++] = nums[fast];
     fast++;
   }
-
   while (slow < len) nums[slow++] = 0;
-
   return nums;
 };

81a4949f5c17aa52daaa74c4b993d2a685b963e4
Jonsam ng <jonsam.ng@foxmail.com>
2022-12-11 18:32:53 +0800

    Time: 127 ms (83.19%), Space: 42.5 MB (96.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -1,12 +1,13 @@
 function binarySearch(arr, target) {
   const len = arr.length;
-  if(!arr.length) return -1;
-  let left = 0, right = len - 1;
-  while(left <= right) {
+  if (!arr.length) return -1;
+  let left = 0,
+    right = len - 1;
+  while (left <= right) {
     const mid = left + ~~((right - left) / 2);
-    if(arr[mid] > target) {
+    if (arr[mid] > target) {
       right = mid - 1;
-    } else if(arr[mid] < target) {
+    } else if (arr[mid] < target) {
       left = mid + 1;
     } else {
       // we find it
@@ -16,19 +17,17 @@ function binarySearch(arr, target) {
   // we want the left pointer which is less bigger than target
   return left;
 }
-/**
- * @param {number[]} nums
- * @return {number}
- */
+
 var lengthOfLIS = function (nums) {
   const len = nums.length;
-  if(!len) return 0;
-  // initize the first num
+  if (!len) return 0;
+  // initiate the first num
   const seq = [nums[0]];
-  for(let i = 1; i < len; i++) {
+  for (let i = 1; i < len; i++) {
     const n = nums[i];
-    if(n > seq.at(-1)) seq.push(n);
-    // we want the seq as small as posible
+    // is not strictly increasing, use >=
+    if (n > seq.at(-1)) seq.push(n);
+    // we want the seq as small as possible
     else {
       const idx = binarySearch(seq, n);
       // idx cannot bigger than tail of seq

e1525d0d05017af1a8c97e5c95e350cc0a2ce1bf
Jonsam ng <jonsam.ng@foxmail.com>
2022-12-11 18:10:28 +0800

    Time: 204 ms (64.85%), Space: 43.9 MB (59.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -4,13 +4,20 @@
  */
 var lengthOfLIS = function (nums) {
   const len = nums.length,
-    dp = new Array(len).fill(1);
+    dp = new Array(len).fill(1),
+    path = [];
   let max = 1;
   for (let i = 0; i < len; i++) {
-    for (let j = 0; j < i; j++) {
+    let j = 0;
+    for (; j < i; j++) {
       if (nums[j] < nums[i]) {
         dp[i] = Math.max(dp[i], dp[j] + 1);
       }
+    }
+    // update path of LIS
+    if (dp[i] > max) {
+      if (max === 1) path.push(nums[j - 1]);
+      path.push(nums[i]);
     }
     // now we know LIS of length i
     max = Math.max(max, dp[i]);

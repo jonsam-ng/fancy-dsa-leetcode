03bba7293d67e6ba89a5136dc7c2b1d07e6f10e5
Jonsam ng <jonsam.ng@foxmail.com>
2022-12-30 16:30:18 +0800

    Time: 85 ms (91.18%), Space: 49.8 MB (95.40%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -1,24 +1,21 @@
 /**
  * @param {number[]} nums
  * @return {number}
+ * @desc by DP
  */
 var maxSubArray = function (nums) {
-  let sum = 0,
-    left = 0,
-    right = 0,
-    rst = -Infinity;
-  
-  while (right < nums.length) {
-    const num = nums[right];
-    
-    right++;
-    sum += num;
-    // make sure temp sum is always larger than num
-    while (sum < num) {
-      sum -= nums[left];
-      left++;
-    }
-    if (sum > rst) rst = sum;
+  const len = nums.length;
+  if (!len) return 0;
+  let m0 = -Infinity,
+    m1 = nums[0],
+    r = m1;
+
+  for (let i = 1; i < len; i++) {
+    const n = nums[i];
+    m0 = m1;
+    m1 = Math.max(n, m0 + n);
+    r = Math.max(r, m1);
   }
-  return rst;
+
+  return r;
 };

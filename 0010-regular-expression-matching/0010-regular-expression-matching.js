6b7bbd05cfbad2b46881b0c69c7dcbe590cca6bc
Jonsam ng <jonsam.ng@foxmail.com>
2023-01-02 17:28:46 +0800

    Time: 79 ms (96.62%), Space: 46.9 MB (42.86%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -1,7 +1,8 @@
 var isMatch = function (s, p) {
   const m = s.length,
     n = p.length,
-    dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(false));
+    dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(false)),
+    isCharMatch = (i, j) => s[i] === p[j] || p[j] === ".";
   dp[0][0] = true;
   // match pattern in the row, and match string to the column
   // we know empty pattern cannot match any string except ''
@@ -11,18 +12,12 @@ var isMatch = function (s, p) {
       // we will consider * as special case, but it cannot be the first letter
       if (j > 1 && p[j - 1] === "*") {
         dp[i][j] =
-          dp[i][j - 2] ||
-          (i > 0 &&
-            dp[i - 1][j] &&
-            (s[i - 1] === p[j - 2] || p[j - 2] === "."));
+          dp[i][j - 2] || (i > 0 && dp[i - 1][j] && isCharMatch(i - 1, j - 2));
       } else {
-        dp[i][j] =
-          i > 0 &&
-          dp[i - 1][j - 1] &&
-          (s[i - 1] === p[j - 1] || p[j - 1] === ".");
+        dp[i][j] = i > 0 && dp[i - 1][j - 1] && isCharMatch(i - 1, j - 1);
       }
     }
   }
-  
+
   return dp[m][n];
 };

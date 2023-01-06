6bc5f49de50a06af63d216e5813732fe0847f292
Jonsam ng <jonsam.ng@foxmail.com>
2023-01-06 16:45:05 +0800

    Time: 87 ms (56.62%), Space: 42.3 MB (25.95%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+  const hash = [],
+    step = 2;
+  let rst = 1,
+    target = Math.abs(n);
+  hash.push([1, x]);
+
+  let count = step,
+    prev = x;
+  while (count < target) {
+    const sum = prev * prev;
+    hash.push([count, sum]);
+    count *= step;
+    prev = sum;
+  }
+
+  while (target > 0) {
+    let idx = 0;
+    for (let i = hash.length - 1; i >= 0; i--) {
+      if (hash[i][0] < target) {
+        idx = i;
+        break;
+      }
+    }
+    rst *= hash[idx][1];
+    target -= hash[idx][0];
+  }
+
+  return n > 0 ? rst : 1 / rst;
+};

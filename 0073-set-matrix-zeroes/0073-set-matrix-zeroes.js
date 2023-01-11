f8ec0b47f4673216fea6c6e6e1d013dc65e6954a
Jonsam ng <jonsam.ng@foxmail.com>
2023-01-11 18:21:55 +0800

    Time: 88 ms (78.20%), Space: 44.6 MB (85.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -4,26 +4,20 @@
  */
 var setZeroes = function (matrix) {
   const m = matrix.length,
-    n = matrix[0].length,
-    rows = [],
-    cols = [];
+    n = matrix[0].length;
+  let flagCol0 = false;
+  
+  for (let i = 0; i < m; i++) {
+    if (matrix[i][0] === 0) flagCol0 = true;
+    for (let j = 1; j < n; j++) {
+      if (matrix[i][j] === 0) matrix[i][0] = matrix[0][j] = 0;
+    }
+  }
 
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (!matrix[i][j]) {
-        if (!rows.includes(i)) rows.push(i);
-        if (!cols.includes(j)) cols.push(j);
-      }
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = 1; j < n; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) matrix[i][j] = 0;
     }
+    if (flagCol0) matrix[i][0] = 0;
   }
-  
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (matrix[i][j] && (rows.includes(i) || cols.includes(j))) {
-        matrix[i][j] = 0;
-      }
-    }
-  }
-  
-  return matrix;
 };
